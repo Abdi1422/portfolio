f6b21db3ba8092fa072e833de3720938a6b9bf78
Clinical1 <abdiaden1422@gmail.com>
2023-01-27 23:01:38 +0000

    deploy

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import './app.scss'
+import Navbar from './components/navbar';
+import { Typography } from '@mui/material';
+import About from './sections/about';
+import Projects from './sections/projects';
+import Contact from './sections/contact';
+import bgimg from './video/backgroundimage.mp4'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className='app'>
+      <video autoPlay loop muted>
+        <source src={bgimg} type="video/mp4"/>
+      </video>
+      <Navbar/>
+      <main className='main' id='home'>
+        <Typography variant='h1' >Hi.I'm Abdi </Typography>
+        <Typography variant='h5' >I am a web developer </Typography>
+        <p>Always learning and impoving</p>
+        <div className='btnwrapper'>
+          <a href='#projects' ><button className='homebtn'>Projects</button></a>
+          <a href='#contact'><button className='homebtn'>Contact</button></a>
+        </div>
+      </main>
+      <About/>
+      <Projects/>
+      <Contact/>
+    </div> 
   );
 }
 
