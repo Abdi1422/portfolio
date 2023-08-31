92d3c953248eb83f219807559b7995b471e52692
Clinical1 <abdiaden1422@gmail.com>
2023-08-31 14:00:21 +0100

    new

--- src/sections/projects.jsx
+++ src/sections/projects.jsx
@@ -3,14 +3,15 @@ import Website from '../components/website'
 import img1 from '../images/gym.png'
 import img2 from '../images/anime.png'
 import img3 from '../images/travel.png'
-import img4 from '../images/home.png'
+import img4 from '../images/footnewpic.png'
+
 
 
 const Projects=()=>{
     const gymtext = "Choose a bodypart and see which exerises to use"
     const animetext ="search which anime you want to watch using anime api"
     const traveltext ="Type which country you want to get information from"
-    const hometext="Showcase of diffrent house and rent prices"
+    const hometext="Showcase of football tables and players"
 
     return(
         <section className='prosection'id='projects'>
@@ -19,12 +20,12 @@ const Projects=()=>{
             </div>
             <div className='webcontainer'>
                 <div className='webwrapper1'>
+                    <Website view={'https://abdi1422.github.io/test/'} name="Footnews" lang={"Typesript / React / Scss"} source="https://github.com/Abdi1422/test" img={img4} text={hometext}/>  
                     <Website view={'https://clinical1.github.io/limitfitness/'} name="Limitfitness" lang={"React / Scss / Mui"} source="https://github.com/Clinical1/limitfitness" img={img1} text={gymtext}/>
-                    <Website view={'https://clinical1.github.io/Animesfree/'} name="Animesfree" lang={"React / Scss "} source="https://github.com/Clinical1/Animesfree" img={img2} text={animetext}/>
                 </div>
                 <div className='webwrapper2'>
-                    <Website view={' https://clinical1.github.io/Travels/'} name="Travels" lang={"React / Scss "} source="https://github.com/Clinical1/Travels" img={img3} text={traveltext}/>
-                    <Website view={'https://clinical1.github.io/property-website/'} name="Homes" lang={"Html / Css / Js"} source="https://github.com/Clinical1/property-website" img={img4} text={hometext}/>    
+                    <Website view={'https://clinical1.github.io/Animesfree/'} name="Animesfree" lang={"React / Scss "} source="https://github.com/Clinical1/Animesfree" img={img2} text={animetext}/>
+                    <Website view={' https://clinical1.github.io/Travels/'} name="Travels" lang={"React / Scss "} source="https://github.com/Clinical1/Travels" img={img3} text={traveltext}/>  
                 </div>
             </div>
         </section>
